ce5cbd60476c1b89273ec355d04da44cad3382c2
Hugues Dubois <hugdubois@gmail.com>
2020-06-23 02:48:25 +0200

    Improved statusbar

--- config.h
+++ config.h
@@ -3,7 +3,7 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
 	/* {"",	"music",	0,	11},*/
-	{"",	"pacpackages",	0,	8},
+	/* {"",	"pacpackages",	0,	8}, */
 	{"",	"news",		0,	6},
 	/* {"",	"crypto",	0,	13}, */
 	/* {"",	"price bat \"Basic Attention Token\" 🦁",	0,	20}, */
@@ -13,14 +13,14 @@ static const Block blocks[] = {
 	/* {"",	"memory",	10,	14}, */
 	/* {"",	"cpu",		10,	18}, */
 	/* {"",	"moonphase",	18000,	17}, */
-	{"",	"weather",	18000,	5},
+	/* {"",	"weather",	18000,	5}, */
 	{"",	"mailbox",	180,	12},
-	/* {"",	"nettraf",	1,	16}, */
+	{"",	"nettraf",	1,	16},
 	{"",	"volume",	0,	10},
 	{"",	"battery",	5,	3},
 	{"",	"clock",	60,	1},
 	{"",	"internet",	5,	4},
-	{"",	"help-icon",	0,	15},
+	/* {"",	"help-icon",	0,	15}, */
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
